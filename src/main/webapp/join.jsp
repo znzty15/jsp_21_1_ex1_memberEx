ac5f9d73098db0e740d0055af869058bf4878161
GoRyeoIn <89904365+znzty15@users.noreply.github.com>
2021-09-30 21:24:49 +0900

    memberEx

--- src/main/webapp/join.jsp
+++ src/main/webapp/join.jsp
@@ -12,9 +12,9 @@
 	<form action="joinOk.jsp" method="post" name="reg_frm">
 		아이디 : <input type="text" name="id" size="20"><br>
 		비밀번호 : <input type="password" name="pw" size="20"><br>
-		비밀번호 확인: <input type="password" name="pw_check" size="20"><br>
-		이름: <input type="text" name="name" size="20"><br>
-		이메일: <input type="text" name="email" size="30"><br>
+		비밀번호 확인 : <input type="password" name="pw_check" size="20"><br>
+		이름 : <input type="text" name="name" size="20"><br>
+		이메일 : <input type="text" name="email" size="30"><br>
 		주소: <input type="text" name="addr" size="60"><br>
 		<input type="button" value="회원가입" onclick="infoConfirm()">
 		<input type="reset" value="취소" onclick="javascript:window.location='login.jsp'">	
--- src/main/webapp/members.js
+++ src/main/webapp/members.js
@@ -4,37 +4,37 @@
  
 function infoConfirm() {
  	
-	if(document.reg_frm.id.value.length==0) {
+	if(document.reg_frm.id.value.length == 0) {
 		alert("아이디는 필수사항입니다.");
 		reg_frm.id.focus();
 		return;
 	}
 	
-	if(document.reg_frm.pw.value.length==0) {
+	if(document.reg_frm.pw.value.length == 0) {
 		alert("비밀번호는 필수사항입니다.");
 		reg_frm.pw.focus();
 		return;
 	}
 	
-	if(document.reg_frm.name.value.length==0) {
+	if(document.reg_frm.name.value.length == 0) {
 		alert("이름은 필수사항입니다.");
 		reg_frm.name.focus();
 		return;
 	}
 	
-	if(document.reg_frm.email.value.length==0) {
+	if(document.reg_frm.email.value.length == 0) {
 		alert("이메일은 필수사항입니다.");
 		reg_frm.email.focus();
 		return;
 	}
 	
-	if(document.reg_frm.id.value.length<4) {
+	if(document.reg_frm.id.value.length < 4) {
 		alert("아이디는 4글자 이상으로 입력하세요.");
 		reg_frm.id.focus();
 		return;
 	}
 	
-	if(document.reg_frm.pw.value.length<5) {
+	if(document.reg_frm.pw.value.length < 5) {
 		alert("비밀번호는 5자 이상으로 입력하세요.");
 		reg_frm.pw.focus();
 		return;
@@ -52,7 +52,7 @@ function infoConfirm() {
 
 function updateInfoConfirm() {
 	
-	if(document.reg_frm.pw.value.length==0) {
+	if(document.reg_frm.pw.value.length == 0) {
 		alert("비밀번호를 입력하세요.");
 		reg_frm.pw.focus();
 		return;
@@ -64,7 +64,7 @@ function updateInfoConfirm() {
 		return;
 	}
 	
-	if(document.reg_frm.email.value.length==0) {
+	if(document.reg_frm.email.value.length == 0) {
 		alert("이메일은 필수사항입니다.");
 		reg_frm.email.focus();
 		return;
